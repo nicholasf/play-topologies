5baf2c6d00c2d81f79df5677c5ca34d5a33eaba0
Nicholas Faiz <nicholas.faiz@gmail.com>
2012-07-01 11:49:57 +1000

    learning curve with clojure and leiningen

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
-(defproject topologies "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject play-topologies "0.0.1-SNAPSHOT"
+  :source-path "src"
+  :javac-options {:debug "true" :fork "true"}
+  :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
+  :aot :all
   :dependencies [[org.clojure/clojure "1.3.0"]
   				[storm "0.8.0-SNAPSHOT"]]
   )
---
+++ src/play_topologies/core.clj
@@ -0,0 +1,50 @@
+(ns play-topologies.core
+	(:import [backtype.storm StormSubmitter LocalCluster])
+	(:use [backtype.storm clojure config])
+	(:gen-class))
+
+(defspout clock-spout ["tick tock"]
+	[conf context collector]
+	(spout 
+		(nextTuple []
+			(Thread/sleep 1000)
+			(emit-spout! collector [(System/currentTimeMillis)])
+			)
+		(ack [id]
+			;;reliable spout
+		)))
+
+(defbolt second-logger ["second" "logger"] {:prepare true}
+	[conf context collector]
+	(bolt
+		(execute [tuple]
+			(let [millis  (.getString tuple 0)]
+				(print "****")
+				(print millis)
+				))))
+(defn mk-topology []
+	(topology
+		{"1" (spout-spec clock-spout)
+		"2" (bolt-spec {"1": shuffle} second-logger : p 2)
+	}))
+
+(defn run-local! []
+  (let [cluster (LocalCluster.)]
+    (.submitTopology cluster "time topology" {TOPOLOGY-DEBUG true} (mk-topology))
+    (Thread/sleep 10000)
+    (.shutdown cluster)
+    ))
+
+(defn submit-topology! [name]
+  (StormSubmitter/submitTopology
+   name
+   {TOPOLOGY-DEBUG true
+    TOPOLOGY-WORKERS 3}
+   (mk-topology)))
+
+(defn -main
+  ([]
+   (run-local!))
+  ([name]
+   (submit-topology! name)))
+
